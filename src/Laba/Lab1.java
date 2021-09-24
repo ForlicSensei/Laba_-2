78a2f9fa0aa45279b09eec2280b3ec965fb74997
Anton Sofronov <231.334@mail.ru>
2021-09-24 22:30:42 +0300

    "Финал проекта"

--- src/Laba/Lab1.java
+++ src/Laba/Lab1.java
@@ -6,7 +6,9 @@ public class Lab1 {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        Point3d x1=(in.nextDouble(),in.)
-
+        Point3d a= new Point3d(in.nextDouble(),in.nextDouble(),in.nextDouble());
+        Point3d b= new Point3d(in.nextDouble(),in.nextDouble(),in.nextDouble());
+        Point3d z= new Point3d(in.nextDouble(),in.nextDouble(),in.nextDouble());
+        System.out.println(Point3d.computeArea(a,b,z));
     }
 }
