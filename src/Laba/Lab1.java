5a4ded7b643dd80b72b8437cc2a99bc08c76fcf6
Anton Sofronov <231.334@mail.ru>
2021-09-24 21:50:36 +0300

    80%

---
+++ src/Laba/Lab1.java
@@ -0,0 +1,12 @@
+package Laba;
+
+import java.util.Scanner;
+
+public class Lab1 {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        Point3d x1=(in.nextDouble(),in.)
+
+    }
+}
